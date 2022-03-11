d0eaca0d4c3edccd1588fb7f5a9d09d8004e29e5
Nick Baez <w0m85k27@protonmail.com>
2022-03-11 13:21:15 -0500

    added seeded users

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const { db } = require("./db");
 const PORT = process.env.PORT || 4000;
 const app = require("./app");
+const seed = require("../seeds");
 
 const init = async () => {
   try {
-    await db.sync()
-    app.listen(PORT, () => console.log(`ALL SERVERS REPORTING FOR DUTY ON ${PORT}`));
+    if ((process.env.SEED = "true")) {
+      await seed();
+    } else {
+      await db.sync();
+    }
+    app.listen(PORT, () =>
+      console.log(`ALL SERVERS REPORTING FOR DUTY ON ${PORT}`)
+    );
   } catch (ex) {
     console.log(ex);
   }
