5903e33299e8a144c46b72efab884bc6db8d8da0
Kenneth Rosas <kencrosas@gmail.com>
2022-03-10 22:17:53 -0500

    key removed

--- client/APICalls/yelp.js
+++ client/APICalls/yelp.js
@@ -1,16 +1,26 @@
 import React from "react";
 import axios from "axios";
 
-export const YelpSeeder = () => {
-  const api = axios.get("https://api.yelp.com/v3/businesses/search", {
-    headers: {
-      Authorization: `Bearer ${"7Sx1VUh0USPACiiBiWX7XGU9IT6E0TPgGnxNHHQIzSiUUl1Kfgdp2HlEFJJ-i0rVrt00nRi3MItgQSwvqLebEa7sRiUFF1Q6_gOXiyEqYzSkGldDEAwbtfD8NQ4pYnYx"}`,
-    },
-    params: {
-      location: "San Diego",
-      categories: "breakfast_brunch",
-    },
-  }).data;
-  console.log(api);
-  return <div></div>;
+export default () => {
+  const apiFunc = () =>
+    axios.get(
+      `${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/search?location=NYC`,
+      {
+        headers: {
+          Authorization: `Bearer ${""}`,
+        },
+        params: {
+          categories: "coffee",
+        },
+      }
+    ).data;
+
+  let boy = apiFunc();
+  console.log(boy);
+
+  return (
+    <div>
+      <h1>Testing Yelp API call</h1>
+    </div>
+  );
 };
