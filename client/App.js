39fd41727ce099f6c89fb6e41faab3934b03b954
DC <sbzdcb@gmail.com>
2022-03-14 21:13:36 -0400

    merged Google

--- client/App.js
+++ client/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Yelp from "./APICalls/yelp";
-import {HomeYelp} from "./Home/HomeYelp";
+import Google from "./APICalls/google";
+import { HomeYelp } from "./Home/HomeYelp";
 import { NavBar } from "./Components/NavBar/NavBar";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { LoginPage } from "./Components/Login/LoginPage";
@@ -20,10 +21,11 @@ export const App = () => {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/homeyelp" element={<HomeYelp/>} />
+          <Route path="/homeyelp" element={<HomeYelp />} />
           <Route path="/" element={<HomeYelp />} />
           <Route path="yelp" element={<Yelp />} />
           <Route path="login" element={<LoginPage />} />
+          <Route path="/google" element={<Google />} />
         </Routes>
       </BrowserRouter>
     </div>
--- server/api/google.js
+++ server/api/google.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const axios = require("axios");
-const { stringify } = require("flatted");
 module.exports = router;
 
 //GET Geocode
