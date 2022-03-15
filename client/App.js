4246c46b6913d80e016f70f939abac764f95f7d5
DC <sbzdcb@gmail.com>
2022-03-14 21:04:28 -0400

    deleted Home

--- client/App.js
+++ client/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Yelp from "./APICalls/yelp";
-import Google from "./APICalls/google";
-
-import Home from "./Home/Home";
+import { HomeYelp } from "./Home/HomeYelp";
+import { NavBar } from "./Components/NavBar/NavBar";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { LoginPage } from "./Components/Login/LoginPage";
+import { me } from "./store";
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    dispatch(me());
+  }, []);
+  const user = useSelector((state) => state.auth);
+
   return (
     <div>
-      <h1>Welcome to OmniGrub!</h1>
-      <Google />
-      <Home />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/homeyelp" element={<HomeYelp />} />
+          <Route path="/" element={<HomeYelp />} />
+          <Route path="yelp" element={<Yelp />} />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
